333f2ff029a502c961f582baa1cc3dd9ae23563a
Bumbobin <ndthung281@gmail.com>
2025-02-21 18:04:50 +0700

    add

--- src/config/axios.main.ts
+++ src/config/axios.main.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-const WEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+// const WEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const WEATHER_API_KEY = "1392b6f71df38aa3d82f032ca44b1c5f";
 
 
 const weatherApi = axios.create({
